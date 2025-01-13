39b009952123f9921ab028b12f3ba0bcffb9eb74
nipunraj09 <103949203+nipunraj09@users.noreply.github.com>
2025-01-13 17:25:21 +0530

    Update etlweather.py

--- etlweather.py
+++ etlweather.py
@@ -11,3 +11,13 @@ LATITUDE = '51.5074'
 LONGITUDE = '-0.1278'
 POSTGRES_CONN_ID='postgres_default'
 API_CONN_ID='open_meteo_api'
+default_args={
+    'owner':'airflow',
+    'start_date':days_ago(1)
+}
+
+## DAG
+with DAG(dag_id='weather_etl_pipeline',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dags:
